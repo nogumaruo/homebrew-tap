0619a6414aba51236f4fb6e4b8d9d4c83714a4df
nogumaruo <98809021+nogumaruo@users.noreply.github.com>
2022-03-23 03:07:32 +0900

    mod version up.

--- Formula/adr-tools-jp.rb
+++ Formula/adr-tools-jp.rb
@@ -1,9 +1,13 @@
 class AdrToolsJp < Formula
   desc "CLI tool for working with Architecture Decision Records JP ver"
   homepage "https://github.com/nogumaruo/adr-tools-jp/"
-  url "https://github.com/nogumaruo/adr-tools-jp/archive/3.0.1.tar.gz"
-  sha256 "193474fa9bbfe9e7abce5f82abd1e040346814bbf34384ea7c4baad55ce9c656"
+  url "https://github.com/nogumaruo/adr-tools-jp/archive/3.0.2.tar.gz"
+  sha256 "bb8fb88edc0e12d3739aa1de5e38b719fdf9c633c301f7ac722a4e47be5044c5"
   license "CC-BY-4.0"
+
+  bottle do
+    sha256 cellar: :any_skip_relocation, arm64_monterey: "bb30126ed24957928cee4df9ef9c4768a928c07fbbf186075941901f16725ef1"
+  end
 
   def install
     config = buildpath/"src/adr-config"
